487ace96ec328cecc6ba79c7806fd411f0623e64
Mats Aadahl <mats.aadahl@esmartsystems.com>
2024-11-11 14:19:12 +0100

    v1.0

---
+++ wezterm.lua
@@ -0,0 +1,37 @@
+local wezterm = require("wezterm")
+
+local config = wezterm.config_builder()
+
+-- colors
+config.color_scheme = "Tokyo Night"
+config.colors = {
+	background = "#141414",
+}
+config.window_background_opacity = 0.85
+
+-- font
+config.font = wezterm.font("Reddit Mono", { weight = "DemiBold" })
+config.font_dirs = { "~/Library/Fonts/" }
+config.font_size = 13
+
+-- keymaps
+local act = wezterm.action
+config.keys = {
+	{
+		key = "E",
+		mods = "CTRL|SHIFT",
+		action = act.PromptInputLine({
+			description = "Enter new name for tab",
+			action = wezterm.action_callback(function(window, pane, line)
+				-- line will be `nil` if they hit escape without entering anything
+				-- An empty string if they just hit enter
+				-- Or the actual line of text they wrote
+				if line then
+					window:active_tab():set_title(line)
+				end
+			end),
+		}),
+	},
+}
+
+return config
