37851afe179a77ec4dacbde2f956535aed6d0ad0
Mats Aadahl <mats.aadahl@esmartsystems.com>
2024-11-15 09:31:55 +0100

    use comic sans mono

--- wezterm.lua
+++ wezterm.lua
@@ -10,9 +10,9 @@ config.colors = {
 config.window_background_opacity = 0.85
 
 -- font
-config.font = wezterm.font("Reddit Mono", { weight = "DemiBold" })
+config.font = wezterm.font("Comic Mono")
 config.font_dirs = { "~/Library/Fonts/" }
-config.font_size = 13
+config.font_size = 12
 
 -- keymaps
 local act = wezterm.action
@@ -23,9 +23,6 @@ config.keys = {
 		action = act.PromptInputLine({
 			description = "Enter new name for tab",
 			action = wezterm.action_callback(function(window, pane, line)
-				-- line will be `nil` if they hit escape without entering anything
-				-- An empty string if they just hit enter
-				-- Or the actual line of text they wrote
 				if line then
 					window:active_tab():set_title(line)
 				end
